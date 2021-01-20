24eb2d9f0b92a362cc27c5ec865a1b935357a5eb
David Lee <DL444@outlook.com>
2021-01-20 13:10:28 +0800

    Add student info refresh function.

---
+++ src/DL444.Ucqu/DL444.Ucqu.Backend/StudentInfoRefresh.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using DL444.Ucqu.Backend.Services;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace DL444.Ucqu.Backend
+{
+    public class StudentInfoRefreshFunction : RefreshFunctionBase
+    {
+        public StudentInfoRefreshFunction(IRefreshFunctionHandlerService refreshService) : base(refreshService) { }
+
+        [FunctionName("StudentInfoRefresh_Client")]
+        public async Task Start(
+            [TimerTrigger("0 0 1 * * *")] TimerInfo timer,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
+        {
+            await StartOrchestratorAsync("StudentInfoRefresh_Orchestrator", starter, log);
+        }
+
+        [FunctionName("StudentInfoRefresh_Orchestrator")]
+        public async Task RunOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            await StartActivityAsync("StudentInfoRefresh_Activity", context);
+        }
+
+        [FunctionName("StudentInfoRefresh_Activity")]
+        public async Task Refresh([ActivityTrigger] string username, ILogger log)
+        {
+            await RefreshService.HandleRequestAsync(
+                username,
+                dataService => dataService.GetStudentInfoAsync(username),
+                (client, context) => client.GetStudentInfoAsync(context),
+                (dataService, resource) => dataService.SetStudentInfoAsync(resource),
+                (oldRes, newRes) => newRes.Name != null,
+                null,
+                log
+            );
+        }
+    }
+}
