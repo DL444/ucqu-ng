cccd0d46abce1e0f89114c76a96291290e0bcc55
David Lee <DL444@outlook.com>
2021-01-23 10:52:02 +0800

    Implement user credential service.

--- src/DL444.Ucqu/DL444.Ucqu.App.WinUniversal/Services/UserCredentialService.cs
+++ src/DL444.Ucqu/DL444.Ucqu.App.WinUniversal/Services/UserCredentialService.cs
@@ -1,27 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Windows.Security.Credentials;
 
 namespace DL444.Ucqu.App.WinUniversal.Services
 {
     internal class UserCredentialService : ICredentialService
     {
-        public string Username => throw new NotImplementedException();
-
-        public string PasswordHash => throw new NotImplementedException();
+        public string Username
+        {
+            get
+            {
+                try
+                {
+                    PasswordCredential credential = vault.FindAllByResource("User").FirstOrDefault();
+                    return credential == null ? null : credential.UserName;
+                }
+                catch (Exception ex) when (ex.HResult == -2147023728)
+                {
+                    return null;
+                }
+            }
+        }
+        public string PasswordHash
+        {
+            get
+            {
+                string username = Username;
+                return username == null ? null : vault.Retrieve("User", Username).Password;
+            }
+        }
 
         public string Token { get; set; }
 
         public void ClearCredential()
         {
-            throw new NotImplementedException();
+            try
+            {
+                foreach (PasswordCredential credential in vault.FindAllByResource("User"))
+                {
+                    vault.Remove(credential);
+                }
+            }
+            catch (Exception ex) when (ex.HResult == -2147023728)
+            {
+                return;
+            }
         }
 
         public void SetCredential(string username, string passwordHash)
         {
-            throw new NotImplementedException();
+            _ = username ?? throw new ArgumentNullException(nameof(username));
+            _ = passwordHash ?? throw new ArgumentNullException(nameof(passwordHash));
+            vault.Add(new PasswordCredential("User", username, passwordHash));
         }
+
+        private PasswordVault vault = new PasswordVault();
     }
 }
