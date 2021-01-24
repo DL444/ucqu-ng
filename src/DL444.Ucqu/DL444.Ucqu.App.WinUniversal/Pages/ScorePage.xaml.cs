7dd37f63f2544d56a4773f16da18b02721fc8426
David Lee <DL444@outlook.com>
2021-01-25 00:05:42 +0800

    Add score set view model and score fetching.

--- src/DL444.Ucqu/DL444.Ucqu.App.WinUniversal/Pages/ScorePage.xaml.cs
+++ src/DL444.Ucqu/DL444.Ucqu.App.WinUniversal/Pages/ScorePage.xaml.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using DL444.Ucqu.App.WinUniversal.Extensions;
+using DL444.Ucqu.App.WinUniversal.Services;
+using DL444.Ucqu.App.WinUniversal.ViewModels;
+using DL444.Ucqu.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +30,36 @@ namespace DL444.Ucqu.App.WinUniversal.Pages
         public ScorePage()
         {
             this.InitializeComponent();
+            localDataService = Application.Current.GetService<IDataService>(x => x.DataSource == DataSource.LocalCache);
+            remoteDataService = Application.Current.GetService<IDataService>(x => x.DataSource == DataSource.Online);
+            cacheService = Application.Current.GetService<ILocalCacheService>();
+            MajorScoreViewModel = new DataViewModel<ScoreSet, ScoreSetViewModel>(new ScoreSetViewModel());
+            SecondMajorScoreViewModel = new DataViewModel<ScoreSet, ScoreSetViewModel>(new ScoreSetViewModel());
         }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Task majorUpdateTask = MajorScoreViewModel.UpdateAsync(
+                () => localDataService.GetScoreAsync(false),
+                () => remoteDataService.GetScoreAsync(false),
+                x => cacheService.SetScoreAsync(x),
+                x => new ScoreSetViewModel(x)
+            );
+            Task secondMajorUpdateTask = MajorScoreViewModel.UpdateAsync(
+                () => localDataService.GetScoreAsync(true),
+                () => remoteDataService.GetScoreAsync(true),
+                x => cacheService.SetScoreAsync(x),
+                x => new ScoreSetViewModel(x)
+            );
+            await Task.WhenAll(majorUpdateTask, secondMajorUpdateTask);
+        }
+
+        internal DataViewModel<ScoreSet, ScoreSetViewModel> MajorScoreViewModel { get; }
+        internal DataViewModel<ScoreSet, ScoreSetViewModel> SecondMajorScoreViewModel { get; }
+
+        private IDataService localDataService;
+        private IDataService remoteDataService;
+        private ILocalCacheService cacheService;
     }
 }
---
+++ src/DL444.Ucqu/DL444.Ucqu.App.WinUniversal/ViewModels/ScoreSetViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DL444.Ucqu.App.WinUniversal.Extensions;
+using DL444.Ucqu.App.WinUniversal.Services;
+using DL444.Ucqu.Models;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace DL444.Ucqu.App.WinUniversal.ViewModels
+{
+    internal struct ScoreSetViewModel
+    {
+        public ScoreSetViewModel(ScoreSet scoreSet)
+        {
+            ILocalizationService locService = Application.Current.GetService<ILocalizationService>();
+            GradePoint = scoreSet.GradePoint;
+            GradePointDisplay = locService.Format("OverallGradePointFormat", scoreSet.GradePoint);
+            Terms = new CollectionViewSource()
+            {
+                IsSourceGrouped = true,
+                ItemsPath = new PropertyPath("Courses"),
+                Source = scoreSet.Terms.Reverse<Term>().Select(x => new TermViewModel(x, locService)).ToList()
+            };
+        }
+
+        public double GradePoint { get; }
+        public string GradePointDisplay { get; }
+
+        public CollectionViewSource Terms { get; }
+    }
+
+    internal struct TermViewModel
+    {
+        public TermViewModel(Term term, ILocalizationService locService)
+        {
+            string termNotation = locService.GetString($"TermNumberNotation{term.TermNumber}");
+            DisplayName = locService.Format("TermDisplayNameFormat", term.BeginningYear, term.EndingYear, termNotation);
+            GradePoint = term.GradePoint;
+            GradePointDisplay = locService.Format("TermGradePointFormat", term.GradePoint);
+            Courses = term.Courses.Select(x => new CourseViewModel(x)).ToList();
+        }
+
+        public string DisplayName { get; }
+        public double GradePoint { get; }
+        public string GradePointDisplay { get; }
+        public List<CourseViewModel> Courses { get; }
+    }
+
+    internal struct CourseViewModel
+    {
+        public CourseViewModel(Course course)
+        {
+            ShortName = course.ShortName;
+            Credit = course.Credit;
+            IsInitialTake = course.IsInitialTake;
+            Score = course.Score;
+            Comment = course.Comment;
+            ShortLecturer = course.ShortLecturer;
+            GradePoint = course.GradePoint;
+        }
+
+        public string ShortName { get; }
+        public double Credit { get; }
+        public bool IsInitialTake { get; }
+        public int Score { get; }
+        public string Comment { get; }
+        public string ShortLecturer { get; }
+        public double GradePoint { get; }
+    }
+}
