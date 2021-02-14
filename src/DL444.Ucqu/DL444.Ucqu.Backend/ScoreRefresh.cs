c5e1b51b8dcfe42832b39994aa6a21eb3870a591
David Lee <DL444@outlook.com>
2021-02-14 16:34:26 +0800

    Make score refresh configurable via app settings.

--- src/DL444.Ucqu/DL444.Ucqu.Backend/ScoreRefresh.cs
+++ src/DL444.Ucqu/DL444.Ucqu.Backend/ScoreRefresh.cs
@@ -19,7 +19,7 @@ namespace DL444.Ucqu.Backend
 
         [FunctionName("ScoreRefresh_Client")]
         public async Task Start(
-            [TimerTrigger("0 */5 0-14,23 * * *")] TimerInfo timer,
+            [TimerTrigger("%Refresh:ScoreRefreshTimer%")] TimerInfo timer,
             [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
